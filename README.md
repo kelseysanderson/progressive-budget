02d0842052044572c4f878553223567f2d298430
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-05-05 14:54:10 -0500

    updated README.md and added screencapture.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# progressive-budget
- pwa assignment
+# Progressive Budget
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Table of Contents
+* [Description](README.md#Description)
+* [Installation](README.md#Installation)
+* [Usage](README.md#Usage)
+* [URL](README.md#URL)
+* [License](README.md#License)
+* [Questions](README.md#Questions)
+
+## Description
+App for users to add expenses and deposits to their budget with or without a connection.
+
+
+## Installation
+To install, clone this from my repository by entering git clone https://github.com/kelseysanderson/progressive-budget.git in a terminal in a new directory. Then, when successfully cloned, type `npm install` in the terminal within that directory.
+
+## Usage
+Following installation, open the terminal in the new directory and enter `npm node server.js` to start the server. Then open your browser and type in localhost:3000 into the address bar in a browser. The live URL can also be found below.
+
+### Screenshot 
+![Screenshot of Progressive Budget](./progressive-budget-screencapture.jpg)
+
+## URL
+[Live URL to Progressive Budget]()
+
+## License
+Read more about the license here:
+https://opensource.org/licenses/MIT
+
+### Questions
+If you have any questions, feel free to contact me:
+
+GitHub: [Kelsey Sanderson GitHub](https://github.com/kelseysanderson)
+
+Email:  [Kelsey Sanderson Email](mailto:kelseymonica@gmail.com)
--- public/db.js
+++ public/db.js
@@ -8,41 +8,30 @@ const indexedDB =
 
 let db;
 
-// Tell indexedDb to open (or create) whatever database you want to work with
-// I'd recommend naming the database something like "budget" and the object store 
-// something like "pending" or "tba"
 const request = indexedDB.open("progressive-budget", 1);
 
-// Set up your object store
-// Think of an object store as a table inside your database
 request.onupgradeneeded = ({ target }) => {
   let db = target.result;
   db.createObjectStore("pending", { autoIncrement: true });
 };
 
-// Leave this code as-is
-// If the request was successful it means the Internet is back up, so we can query the real database.
 request.onsuccess = ({ target }) => {
   db = target.result;
-  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
 };
 
-// Simple error handler. Leave as-is
 request.onerror = function(event) {
   console.log("Woops! " + event.target.errorCode);
 };
 
-// This function is called when it's time to save data to the indexedDb
 function saveRecord(record) {
   const transaction = db.transaction(["<object store name here>"], "readwrite");
   const store = transaction.objectStore("<object store name here>");
   store.add(record);
 }
 
-// This function runs when we detect that the internet connection is working again. It sends a post request to the server with all the saved data so that the data can be synced with the server, and then it wipes out the existing indexedDb. You can keep as-is, unless you want to change the name of the fetch route.
 function checkDatabase() {
   const transaction = db.transaction(["<object store name here>"], "readwrite");
   const store = transaction.objectStore("<object store name here>");
@@ -62,7 +51,6 @@ function checkDatabase() {
         return response.json();
       })
       .then(() => {
-        // delete records if successful
         const transaction = db.transaction(["<object store name here>"], "readwrite");
         const store = transaction.objectStore("<object store name here>");
         store.clear();
@@ -71,5 +59,4 @@ function checkDatabase() {
   };
 }
 
-// listen for app coming back online
 window.addEventListener("online", checkDatabase);
